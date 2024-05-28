cc001092f5271adbad6e5390444461b9418c186a
Your Name <manishraghav95964@gmail.com>
2024-05-28 21:16:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-[100vh]' >
+      <h1 className='text-purple-500 font-bold text-2xl'> 
+      Most welcome everyone
+      </h1>
+       </div>
+    </>
+  )
+}
+
+export default App
