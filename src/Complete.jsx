fec6925b8cbd2178132b2740ea9e378399195f56
harsh <harshu20a>
2024-06-03 13:03:33 +0530

    project

---
+++ src/Complete.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import Data from "./Data"; // Import Data component
+
+function Complete({ onCardClick }) {
+  const data = [
+    {
+      name: "user flow social Application design",
+      id: 1001,
+      pr_manager: "Neilson Mando",
+      Start_date: "01-02-2023",
+      Finish_date: "05-03-2023",
+      status: "Complete",
+      Description: "It is Complete",
+    },
+    // Add more data if needed
+  ];
+
+  const color = {
+    color: "green",
+    backgroundColor: "lightGreen",
+  };
+
+  const handleCardClick = (name) => {
+    onCardClick(name);
+  };
+
+  return (
+    <div>
+      <Data onCardClick={onCardClick} /> {/* Add Data component */}
+      <div>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>S.N.</th>
+              <th>Project Name</th>
+              <th>Project Id</th>
+              <th>Project Manager</th>
+              <th>Start Date</th>
+              <th>Finish Date</th>
+              <th>Status</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((pr, index) => (
+              <tr key={pr.id} onClick={() => handleCardClick(pr.name)}>
+                <td>{index + 1}</td>
+                <td>{pr.name}</td>
+                <td>{pr.id}</td>
+                <td>{pr.pr_manager}</td>
+                <td>{pr.Start_date}</td>
+                <td>{pr.Finish_date}</td>
+                <td style={color}>{pr.status}</td>
+                <td>{pr.Description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+}
+
+export default Complete;
