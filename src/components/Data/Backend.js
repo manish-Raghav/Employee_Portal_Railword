91edc4820f45e110b383b60dc1ce6354d44536aa
Naman Pahariya <namanpahariya21@gmail.com>
2024-06-08 21:17:41 +0530

    inital commit

---
+++ src/components/Data/Backend.js
@@ -0,0 +1,164 @@
+export const Back =[
+    {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+  
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+      {
+        emp_id :'RML33012',
+        name:'Vinod Kambale',
+        Department:'Design',
+        Status:'permanent',
+        img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+  
+      },
+        {
+          emp_id :'RWI1231',
+          name:'Manish Thakur',
+          Department:'Design',
+          Status:'permanent',
+          img:'https://www.shutterstock.com/image-photo/close-smiling-single-young-bearded-260nw-385845121.jpg'
+    
+        },
+  
+        {
+          emp_id :'RWI1231',
+          name:'Manish Thakur',
+          Department:'Design',
+          Status:'permanent',
+          img:'https://www.shutterstock.com/image-photo/close-smiling-single-young-bearded-260nw-385845121.jpg'
+    
+        },
+        {
+            emp_id :'RML33012',
+            name:'Vinod Kambale',
+            Department:'Design',
+            Status:'permanent',
+            img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+      
+          },
+  
+          {
+            emp_id :'RML33012',
+            name:'Vinod Kambale',
+            Department:'Design',
+            Status:'permanent',
+            img:'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D'
+      
+          }, {
+          emp_id :'RWI1231',
+          name:'Manish Thakur',
+          Department:'Design',
+          Status:'permanent',
+          img:'https://www.shutterstock.com/image-photo/close-smiling-single-young-bearded-260nw-385845121.jpg'
+    
+        },
+]
